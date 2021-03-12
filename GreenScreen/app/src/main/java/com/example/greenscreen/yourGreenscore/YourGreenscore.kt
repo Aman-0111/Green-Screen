1ed3f615bb1025bbdd9fa467307198095a14d6d9
Sebastian Dan <39214352+Sevixdd@users.noreply.github.com>
2021-03-12 12:39:12 +0000

    YourGreenscore Works!!

--- GreenScreen/app/src/main/java/com/example/greenscreen/yourGreenscore/YourGreenscore.kt
+++ GreenScreen/app/src/main/java/com/example/greenscreen/yourGreenscore/YourGreenscore.kt
@@ -16,17 +16,17 @@ class yourGreenscore : AppCompatActivity() {
         val range = Range()
         range.color = Color.parseColor("#ce0000")
         range.from =0.0
-        range.to = -50.0
+        range.to = 50.0
 
         val range2 = Range()
         range2.color = Color.parseColor("#E3E500")
-        range2.from = -50.0
-        range2.to = -100.0
+        range2.from = 50.0
+        range2.to = 100.0
 
         val range3 = Range()
         range3.color = Color.parseColor("#00b20b")
-        range3.from = -100.0
-        range3.to = -150.0
+        range3.from = 100.0
+        range3.to = 150.0
 
         //add color ranges to gauge
         //add color ranges to gauge
@@ -37,8 +37,8 @@ class yourGreenscore : AppCompatActivity() {
         //set min max and current value
         //set min max and current value
         halfGauge.minValue = 0.0
-        halfGauge.maxValue = (-150).toDouble()
-        halfGauge.value = (-80).toDouble()
+        halfGauge.maxValue = (150).toDouble()
+        halfGauge.value = (80).toDouble()
 
         halfGauge.setNeedleColor(Color.DKGRAY)
         halfGauge.valueColor = Color.BLUE
@@ -46,25 +46,6 @@ class yourGreenscore : AppCompatActivity() {
         halfGauge.maxValueTextColor = Color.GREEN
 
 
-        /*
-         val range = Range()
-         range.color = Color.parseColor("#ce0000")
-         range.from = 0.0
-         range.to = 50.0
-         val range2 = Range()
-         range2.color = Color.parseColor("#E3E500")
-         range2.from = 50.0
-         range2.to = 100.0
-         val range3 = Range()
-         range3.color = Color.parseColor("#00b20b")
-         range3.from = 100.0
-         range3.to = 150.0
-         halfGauge.addRange(range)
-         halfGauge.addRange(range2)
-         halfGauge.addRange(range3)
-         halfGauge.minValue = 0.0
-         halfGauge.maxValue = 150.0
-         halfGauge.value = 0.0*/
 
 
         half_gauge_update_btn.setOnClickListener {
